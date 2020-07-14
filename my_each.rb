9b34c144d4cf1885bcfb754ad61db7d3bad1d836
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-14 20:10:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,8 @@
 words = ['hi', 'hello', 'bye', 'goodbye']
 def my_each(words) do |i|
   puts i
-while i < array.length
-    yield array[i]
+while i < words.length
+    yield words[i]
     i = i + 1
   end
-end
-
-  i = 0
- 
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
- 
-  array
 end
