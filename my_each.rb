a16cd0ceff4eb191b347883072da51ab589b74eb
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-14 21:18:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ words = ['hi', 'hello', 'bye', 'goodbye']
 def my_each(words)
   if my_each(words) do |word|
     puts word
-  else i < words.length
+  elsif i < words.length
     yield (words[i])
   end
 end
